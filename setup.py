846eb58c672d860db4f75fb15c8be2d8211ed684
Snail <theepicsnail@gmail.com>
2024-09-15 11:59:02 -0700

    Set an actual version number.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='vrchat_oscquery',
-    version='0.1dev0',
+    version='1.0.0',
     author='snail', 
     author_email='vrchat_oscquery@snail.rocks',
     packages=find_packages(),
